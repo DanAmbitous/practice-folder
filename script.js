158be17c5250f0ac9a943917cf5fdd13a1160b7a
Danial A <danazad2016@gmail.com>
2021-07-23 12:50:54 -0500

    Further async/await learnings

--- script.js
+++ script.js
@@ -20,24 +20,65 @@ function getValueWithDelayError(value, delay) {
 //Then call getValueWith Delayerror and make sure the error is well caught
 
 // Traditional Promise syntax
-getValueWithDelay("Danial", 250).then((message) => console.log(message))
-getValueWithDelay("Дщыфцшоася", 250)
-  .then((message) => console.log(message))
-  .catch((e) => console.log(e))
-  .finally(() => console.log("ывфаыфуй"))
+// getValueWithDelay("Danial", 250).then((message) => console.log(message))
+// getValueWithDelay("Дщыфцшоася", 250)
+//   .then((message) => console.log(message))
+//   .catch((e) => console.log(e))
+//   .finally(() => console.log("ывфаыфуй"))
 
-// Async/Await syntax
-async function catching() {
-  try {
-    const returnTheResolve = await getValueWithDelay("hi", 250)
-    console.log(returnTheResolve)
-    const returnTheResolve2 = await getValueWithDelay("La tierra", 250)
-    console.log(returnTheResolve2)
-  } catch (error) {
-    console.log(error)
-  } finally {
-    console.log("سلام")
+// // Async/Await syntax
+// async function catching() {
+//   try {
+//     const returnTheResolve = await getValueWithDelay("hi", 250)
+//     console.log(returnTheResolve)
+//     const returnTheResolve2 = await getValueWithDelay("La tierra", 250)
+//     console.log(returnTheResolve2)
+//   } catch (error) {
+//     console.log(error)
+//   } finally {
+//     console.log("سلام")
+//   }
+// }
+
+// catching()
+
+async function loopiation() {
+  for (let i = 0; i <= 10; i++) {
+    try {
+      const laRespuesta = await getValueWithDelay(
+        "No es sabio alabar el día",
+        100
+      )
+      console.log(laRespuesta)
+      const laRespuesta2 = await getValueWithDelay(
+        "No es sabio alabar el día",
+        100
+      )
+      console.log(laRespuesta2)
+      const laRespuesta3 = await getValueWithDelay(
+        "No es sabio alabar el día",
+        100
+      )
+      console.log(laRespuesta3)
+      const laRespuesta4 = await getValueWithDelayError(
+        "No es sabio alabar el día",
+        100
+      )
+      console.log(laRespuesta4)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      console.log(
+        "Por fin mi compañeros, hemos llegado antes de la puesta del sol"
+      )
+    }
   }
 }
 
-catching()
+loopiation()
+
+// for (let i = 0; i <= 10; i++) {
+//   getValueWithDelay("Abrelo", 100).then((message) => {
+//     console.log(message)
+//   })
+// }
