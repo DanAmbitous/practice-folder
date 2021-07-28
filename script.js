f36622f0dd88c5f462c6662896db3566c0812ef9
Danial A <danazad2016@gmail.com>
2021-07-28 15:02:01 -0500

    Added extra functionalities

--- script.js
+++ script.js
@@ -2,6 +2,29 @@
   plugins: ["jsdom-quokka-plugin"],
 })
 
-const calenderButtonToggler = document.querySelector(".date-picker-button")
+import { format, getUnixTime, fromUnixTime } from "date-fns"
 
-console.log(calenderButtonToggler)
+const calenderButtonToggler = document.querySelector(".date-picker-button")
+const calenderElement = document.querySelector(".date-picker")
+const calenderHeaderElement = document.querySelector(".current-month")
+
+function calenderToggler() {
+  calenderElement.classList.toggle("show")
+  const currentDate = fromUnixTime(calenderButtonToggler.dataset.selectedDate)
+  setupDatePicker(currentDate)
+}
+
+function currentDateAssigner(date) {
+  calenderButtonToggler.innerHTML = format(date, "MMMM do, y")
+  calenderButtonToggler.dataset.selectedDate = getUnixTime(date)
+}
+
+function setupDatePicker(selectedDate) {
+  calenderHeaderElement.innerHTML = format(selectedDate, "MMMM - Y")
+}
+
+calenderButtonToggler.addEventListener("click", () => {
+  calenderToggler()
+})
+
+currentDateAssigner(new Date())
