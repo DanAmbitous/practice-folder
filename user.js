eb1bf4a8d15bd88d6956e16612d6ecaf3c941ea8
Danial A <danazad2016@gmail.com>
2021-07-30 11:55:00 -0500

    Starting on the date picker

---
+++ user.js
@@ -0,0 +1,15 @@
+export const me = {
+  name: "Danial",
+  age: 16,
+}
+
+export const nabati = {
+  name: "Nabati",
+  age: 16,
+}
+
+function printUser(user) {
+  console.log(`Name: ${user.name}. Age: ${user.age}`)
+}
+
+export default printUser
