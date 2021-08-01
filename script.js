6f6630a013a0811b2d5964383c930d03a0b1d393
Danial A <danazad2016@gmail.com>
2021-08-01 13:41:24 -0500

    Added navigational functionalities to the calender program

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ import {
   fromUnixTime,
   addMonths,
   subMonths,
+  eachDayOfInterval,
+  startOfWeek,
+  startOfMonth,
+  endOfWeek,
+  endOfMonth,
+  isSameMonth,
+  isSameDay,
 } from "date-fns"
 
 const calenderButton = document.querySelector(".date-picker-button")
 const calenderElement = document.querySelector(".date-picker")
 const calenderHeaderText = document.querySelector(".current-month")
+const calenderGrid = document.querySelector(".date-picker-grid-dates")
 const nextCalenderButton = document.querySelector(".next-month-button")
 const previousCalenderButton = document.querySelector(".prev-month-button")
 let currentDate = new Date()
@@ -16,12 +24,42 @@ let currentDate = new Date()
 calenderButton.addEventListener("click", () => {
   calenderElement.classList.toggle("show")
   const selectedDate = fromUnixTime(calenderButton.dataset.selectedData)
-
+  currentDate = selectedDate
   setupDatePicker(selectedDate)
 })
 
 function setupDatePicker(date) {
   calenderHeaderText.innerText = format(date, "MMMM - Y")
+  setupDates(date)
+}
+
+function setupDates(selectedDate) {
+  const firstDayOfWeek = startOfWeek(startOfMonth(selectedDate))
+  const lastDayOfWeek = endOfWeek(endOfMonth(selectedDate))
+  const dates = eachDayOfInterval({ start: firstDayOfWeek, end: lastDayOfWeek })
+
+  calenderGrid.innerHTML = ""
+
+  dates.forEach((date) => {
+    const dateElement = document.createElement("button")
+    dateElement.classList.add("date")
+    dateElement.innerHTML = date.getDate()
+
+    if (!isSameMonth(date, selectedDate)) {
+      dateElement.classList.add("date-picker-other-month-date")
+    }
+
+    if (isSameDay(date, selectedDate)) {
+      dateElement.classList.add("selected")
+    }
+
+    dateElement.addEventListener("click", () => {
+      setDate(date)
+      setupDatePicker(date)
+    })
+
+    calenderGrid.append(dateElement)
+  })
 }
 
 function setDate(date) {
